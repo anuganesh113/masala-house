9967cba25d34c70ff23f59560e41c8622b6a5df5
sanam <shtsomeone@gmail.com>
2026-03-08 11:35:49 +0545

    fixes

--- app/Services/WebsiteService.php
+++ app/Services/WebsiteService.php
@@ -14,6 +14,7 @@ use App\Models\Menu;
 use App\Models\Page;
 use App\Models\Popup;
 use App\Models\Service;
+use App\Models\Setting;
 use App\Models\Testimonial;
 
 /**
@@ -25,13 +26,20 @@ class WebsiteService
     {
         $data['about'] = getPageBySlug('about');
         $data['wonderful_dining'] = getPageBySlug('wonderful-dining');
-     $data['categories'] = Category::with(['menus' => function ($query) {
-    $query->status();
-     }])->get();
+        $data['categories'] = Category::with(['menus' => function ($query) {
+            $query->status();
+        }])->get();
         $data['galleries'] = Gallery::get();
         $data['popup'] = Popup::where('status', Status::ACTIVE)->first();
 
 
+        return $data;
+    }
+
+
+    public function settings()
+    {
+        $data = Setting::first();
         return $data;
     }
 
@@ -47,11 +55,11 @@ class WebsiteService
                 $data['welcome'] = getPageBySlug('welcome-to-masala');
                 $data['dining_experience'] = getPageBySlug('dining-experiences');
                 $data['members'] = MemberMessage::query()->get();
-                  $data['galleries'] =  Album::with('gallery')->get();
+                $data['galleries'] =  Album::with('gallery')->get();
                 $data['compliments'] = Testimonial::query()
                     ->with(['member:id,name,designation'])
                     ->whereNotNull('member_message_id')
-                    ->select(['id','member_message_id','name','designation','message'])
+                    ->select(['id', 'member_message_id', 'name', 'designation', 'message'])
                     ->where('status', Status::ACTIVE)
                     ->inRandomOrder()->take(5)
                     ->get();
@@ -59,10 +67,10 @@ class WebsiteService
 
             case 'blogs':
                 $data['categories'] = Category::query()->get();
-                $data['blogs'] = Blog::query()->select(['id','tag','name','slug','image'])->paginate(Pagination::MEDIUM_PAGE);
+                $data['blogs'] = Blog::query()->select(['id', 'tag', 'name', 'slug', 'image'])->paginate(Pagination::MEDIUM_PAGE);
                 $data['compliments'] = Testimonial::query()
                     ->whereNotNull('member_message_id')
-                    ->select(['id','name','designation','message'])
+                    ->select(['id', 'name', 'designation', 'message'])
                     ->get();
                 break;
 
@@ -77,17 +85,17 @@ class WebsiteService
             case 'menu':
                 $data['menus'] = Menu::query()->status()->get();
                 $data['categories'] = Category::with(['menus' => function ($query) {
-    $query->status();
-     }])->get();
+                    $query->status();
+                }])->get();
                 break;
 
             case 'catering':
                 $data['service'] = getPageBySlug('services');
                 $data['services'] = Service::query()->get();
-                   $data['compliments'] = Testimonial::query()
+                $data['compliments'] = Testimonial::query()
                     ->with(['member:id,name,designation'])
                     ->whereNotNull('member_message_id')
-                    ->select(['id','member_message_id','name','designation','message'])
+                    ->select(['id', 'member_message_id', 'name', 'designation', 'message'])
                     ->where('status', Status::ACTIVE)
                     ->inRandomOrder()->take(5)
                     ->get();
--- resources/views/site/mail/contact.blade.php
+++ resources/views/site/mail/contact.blade.php
@@ -1,55 +1,66 @@
-<!DOCTYPE html>
 <html>
-<head>
-    <title>Masala House</title>
-</head>
 
-<body>
-    <h1>{{ $details['request_name'] ?? 'Contact Us' }}</h1>
-    <div class="row">
-    	<div class="col-md-2">
-    		<p><b>Name:</b>{{ $details['name'] ?? "N/A" }}</p>
-    	</div>
-    	
-    </div>
-    <div class="row">
-    	<div class="col-md-2">
-    		<p><b>Email:</b>{{ $details['email'] ?? "N/A" }}</p>
-    	</div>
-    	
-    </div>
-     <div class="row">
-    	<div class="col-md-2">
-    		<p><b>Phone:</b>{{ $details['phone'] ?? "N/A" }}</p>
-    	</div>
-    	
-    </div>
-    
-    <div class="row">
-    	<div class="col-md-2">
-    		<p><b>Date:</b>{{ $details['date'] ?? "N/A" }}</p>
-    	</div>
-    	
-    </div>
-    <div class="row">
-    	<div class="col-md-2">
-    		<p><b>Time:</b>{{ $details['time'] ?? "N/A" }}</p>
-    	</div>
-    	
-    </div>
-     <div class="row">
-    	<div class="col-md-2">
-    		<p><b>Persons:</b>{{ $details['persons'] ?? "N/A" }}</p>
-    	</div>
-    </div>
-      <div class="row">
-    	<div class="col-md-2">
-    		<p><b>Message:</b>{{ $details['message'] ?? "N/A" }}</p>
-    	</div>
-    </div>
+<body style="background-color:#e2e1e0;font-family: Open Sans, sans-serif;font-size:100%;font-weight:400;line-height:1.4;color:#000;">
+	<table style="max-width:670px;margin:50px auto 10px;background-color:#fff;padding:50px;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;-webkit-box-shadow:0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24);-moz-box-shadow:0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24);box-shadow:0 1px 3px rgba(0,0,0,.12),0 1px 2px rgba(0,0,0,.24); border-top: solid 10px #ff5353;">
+		<thead>
+			<tr>
+				<th style="text-align:left;"><img style="max-width: 150px;" src="{{asset('site-assets/images/logo-color.png')}}"
+						alt=""></th>
+				<th style="text-align:right;font-weight:400; font-size: 15px;">{{\Carbon\Carbon::now()->format('l, d F, Y')}}</th>
+			</tr>
+		</thead>
 
-    
-    
-    <p>Thank you</p>
+		<tbody>
+			<tr>
+				<td style="height:35px;"></td>
+			</tr>
+			<tr>
+				<td colspan="2" style="font-size:20px;padding:30px 15px 0 15px;">Hello Customer</td>
+			</tr>
+			<!-- <tr>
+				<td colspan="2" style="padding:15px;">
+					<p style="font-size:14px;margin:0;">
+						<span style="display:block;font-size:13px;font-weight:normal;"> Your order has been placed successfully with ORDER ID:.</span>
+					</p>
+				</td>
+			</tr> -->
+			<tr>
+				<td colspan="2" style="padding:15px;">
+					<p style="font-size:14px;margin:0;">
+						<span style="display:block;font-size:13px;font-weight:normal;text-align: center;">
+							Thank you for choosing us.</span>
+					</p>
+				</td>
+			</tr>
+
+			<tr>
+				<td>
+					<table style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+						<tr>
+							<td style="vertical-align: top; padding: 0 2% 0 3%;">
+								@foreach($details['data'] as $key => $value)
+								{{ucfirst($key)}}: <span style="font-size: small;">{{$value}}</span> <br>
+								@endforeach
+							</td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+		</tbody>
+		<tfooter>
+			<tr>
+				<td colspan="2" style="font-size:14px;padding:20px 15px 0 15px;">
+					<strong style="display:block;margin:0 0 6px 0;">Regards,</strong>
+					<strong style="padding-bottom: 6px; display: inline-block;">Customer Service</strong><br>
+					Masala House<br><br>
+					<b>Phone:</b> {{$details['phone'] ?? '(525) 847-0411'}}<br>
+					<b>Address:</b> {{$details['address'] ?? ''}}<br>
+					<b>Email:</b> {{$details['siteemail'] ?? 'sales@gmail.com'}}<br>
+					<b>Web:</b> <a href="#!"></a>
+				</td>
+			</tr>
+		</tfooter>
+	</table>
 </body>
+
 </html>
