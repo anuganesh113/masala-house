ecf26083874093d67b5fa2d902db943e86186147
sanam <shtsomeone@gmail.com>
2026-03-15 17:25:10 +0545

    scroll

--- resources/views/site/includes/header.blade.php
+++ resources/views/site/includes/header.blade.php
@@ -7,6 +7,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <meta name="google-site-verification" content="PVElXv82RpomDLSiiyhvSw8In_ACNXiFC_y-l6RwEXA" />
+<!-- Google Tag Manager --> 
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], 
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); 
+})(window,document,'script','dataLayer','GTM-K8WJ7LSJ');</script> 
+<!-- End Google Tag Manager -->
+
   @stack('seo-share')
 
   <title>@yield('page_title')</title>
