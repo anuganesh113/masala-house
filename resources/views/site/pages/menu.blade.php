3e528841669aebcc701c713399cbbb2cf621abe9
sanam <shtsomeone@gmail.com>
2026-03-23 16:39:49 +0545

    fixes

--- resources/views/site/pages/menu.blade.php
+++ resources/views/site/pages/menu.blade.php
@@ -1,4 +1,9 @@
-@extends('site.layouts.layout')
+@extends('site.layouts.layout',[
+    'title' =>   data_get($page, "seo.title") ?? 'Menu',
+    'description' =>  strip_tags(data_get($page, "seo.description") ?? description()),
+    'image' =>   $page ?  $page->full_image_link : banner() ,
+    'keywords' =>  data_get($page, "seo.keywords") ?? keywords(),
+])
 @push('header')
 <style>
     .itemgrab__box{
@@ -47,7 +52,7 @@
                             </ul>
                         </div>
                     </div>
-
+                    
                     <!-- <div class="col-xl-3">
                         <a href="{{ requesturl() }}" class="l__button l__button--primary">
                             <i class='bx bx-carts'></i> Order Now
