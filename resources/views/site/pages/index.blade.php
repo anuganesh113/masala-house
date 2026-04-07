3e4953637f0e1e70efce58816953ae050dc09114
sanam <shtsomeone@gmail.com>
2026-04-07 15:03:43 +0545

    fixes

--- resources/views/site/pages/index.blade.php
+++ resources/views/site/pages/index.blade.php
@@ -35,8 +35,9 @@
         <div class="row">
             <div class="col-lg-7 offset-lg-5 col-xl-6 offset-xl-6">
                 <div class="section__title">
-                    <h4>Since 2015</h4>
-                    <h2> Wonderful Dining Experience & Indian Food</h2>
+               
+                    <h4> {{ $about->metadata['sub_heading'] ?? 'Since 2015' }} </h4>
+                    <h2> {{ $about->metadata['sub_title'] ?? 'Wonderful Dining Experience & Indian Food' }} </h2>
                 </div>
                 <div class="content">
                     {!!   $about->excerpt ??  $about->description !!}
