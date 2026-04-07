78a20afbbd67d3e3f598947803e01e2c047af36b
sanam <shtsomeone@gmail.com>
2026-04-07 17:00:45 +0545

    css

--- resources/views/site/includes/header.blade.php
+++ resources/views/site/includes/header.blade.php
@@ -35,13 +35,13 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
   <!-- bootstrap -->
-  <link href="{{ asset('site-assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+  <link href="{{ asset('site-assets/css/bootstrap.min.css?v1.1') }}" rel="stylesheet" type="text/css" />
   <!-- owl carousel -->
   <link href="{{ asset('site-assets/css/owl.carousel.min.css') }}" rel="stylesheet" type="text/css" />
   <link href="{{ asset('site-assets/css/owl.theme.default.min.css') }}" rel="stylesheet" type="text/css" />
   <!-- css -->
-  <link rel="stylesheet" href="{{ asset('site-assets/css/style.css?v1.2') }}">
-  <link rel="stylesheet" href="{{ asset('site-assets/css/override.css?v1.2') }}">
+  <link rel="stylesheet" href="{{ asset('site-assets/css/style.css?v1.1') }}">
+  <link rel="stylesheet" href="{{ asset('site-assets/css/override.css?v1.1') }}">
 
 
 
