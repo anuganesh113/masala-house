deb04f7a5e3a45c428c5f9e59806540090a31efb
sanam <shtsomeone@gmail.com>
2026-04-22 17:59:10 +0545

    search

--- resources/views/admin/pages/categories/index.blade.php
+++ resources/views/admin/pages/categories/index.blade.php
@@ -37,7 +37,7 @@
                             <div class="col-md-8">
                                 <div class="m-input-icon m-input-icon--left">
                                     <input type="text" class="form-control m-input"
-                                        placeholder="Search..." class="generalrowSearch">
+                                        placeholder="Search..." id="generalSearch">
                                     <span class="m-input-icon__icon m-input-icon__icon--left">
                                         <span>
                                             <i class="la la-search"></i>
@@ -149,7 +149,7 @@
             }
 
             // Quick search functionality
-            $(".generalrowSearch").on("keyup", function() {
+            $("#generalSearch").on("keyup", function() {
                 var value = $(this).val().toLowerCase();
                 $("#tablecontents tr").filter(function() {
                     $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
