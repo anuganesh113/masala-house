03a8acf761776db819422bd26f33774d6f8809a4
sanam <shtsomeone@gmail.com>
2026-04-30 17:16:04 +0545

    justify-content-center

--- resources/views/site/pages/blogs.blade.php
+++ resources/views/site/pages/blogs.blade.php
@@ -31,7 +31,7 @@
         </div>
     </div>
     <div class="container-fluid">
-        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
+        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4  justify-content-center">
 
             @foreach($blogs ?? [] as $blog)
                 <div class="col">
