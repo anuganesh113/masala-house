5b2d8ee78c9af832acaadca76e3b803a382152ce
sanam <shtsomeone@gmail.com>
2026-05-04 16:07:17 +0545

    instagram-media

---
+++ resources/views/site/pages/instagram-media.blade.php
@@ -0,0 +1,22 @@
+@push('header')
+@media (max-width: 767px) {
+
+}
+
+@endpush
+
+<!-- <section style="    background-color: #1d1d1e;">
+<div class="container-fluid">
+    <div class="row mt-3">
+        <div class="col-md-12 col-sm-12 w-100" id="instagram-media" style="padding: 0 200px;">
+            <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/masala_housepittsburgca" data-instgrm-version="xx">
+                <div class="instagram-media-inner">
+                    <a href="https://www.instagram.com/masala_housepittsburgca" target="_blank"></a>
+                </div>
+            </blockquote>
+            <script async src="//www.instagram.com/embed.js"></script>
+        </div>
+    </div>
+</div>
+</section> -->
+
